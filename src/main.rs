99129d084798d89d67d638ec8d2b1dc4372abdfa
Stef <stef.robbe@gmail.com>
2021-09-26 10:53:42 +0200

    CLI version finished

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-use winapi::um::processthreadsapi::{TerminateProcess, OpenProcess};
-use winapi::um::winnt::PROCESS_ALL_ACCESS;
-use winapi::um::tlhelp32::{Process32NextW, Process32FirstW, CreateToolhelp32Snapshot, TH32CS_SNAPPROCESS, PROCESSENTRY32W};
-use winapi::um::winnt::HANDLE;
-use winapi::um::handleapi::CloseHandle;
+use std::io;
 use std::mem::size_of;
-use std::os::windows::ffi::OsStringExt;
 use std::ffi::OsString;
-// use winapi::ctypes::{__int64, __uint64, c_char, c_int, c_long, c_short, c_uint, c_ulong, c_void, wchar_t};
-// use winapi::shared::minwindef::{
-//     BOOL, DWORD, LPBYTE, LPCVOID, LPDWORD, LPFILETIME, LPVOID, PBOOL, PDWORD, PULONG, UINT, WORD
-// };
+use structopt::StructOpt;
+use winapi::um::winnt::HANDLE;
+use winapi::um::winnt::PROCESS_ALL_ACCESS;
+use winapi::um::handleapi::CloseHandle;
+use std::os::windows::ffi::OsStringExt;
+use winapi::um::processthreadsapi::{TerminateProcess, OpenProcess};
+use winapi::um::tlhelp32::{Process32NextW, Process32FirstW, CreateToolhelp32Snapshot, TH32CS_SNAPPROCESS, PROCESSENTRY32W};
 
-unsafe fn kill_process() -> HANDLE {
+#[derive(Debug)]
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(default_value = "")]
+    process_name: String,
+}
+
+unsafe fn kill_process(process_id: u32) -> HANDLE {
     let h_process: HANDLE;
 
     // Powershell: "Get-Process"
     // example, spotify Id  = 1736...
     // 419 | 53 | 63152 | 105304 | 16.20 | 1488 | 1 | Spotify
-    h_process = OpenProcess(PROCESS_ALL_ACCESS, 0, 1736);
+    h_process = OpenProcess(PROCESS_ALL_ACCESS, 0, process_id);
 
-    println!("{:?}", h_process);
+    // println!("{:?}", h_process);
 
     TerminateProcess(h_process, 1);
 
@@ -28,9 +33,9 @@ unsafe fn kill_process() -> HANDLE {
     h_process
 }
 
-fn get_processes() {
+fn get_all_processes() {
     let h_process_snap: HANDLE;
-    let mut pe32 = &mut PROCESSENTRY32W{
+    let mut pe32 = &mut PROCESSENTRY32W {
         dwSize: 0,
         cntUsage: 0,
         th32ProcessID: 0,
@@ -46,10 +51,8 @@ fn get_processes() {
     unsafe {
         h_process_snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     }
-    println!("{:?}", h_process_snap);
 
     pe32.dwSize = size_of::<PROCESSENTRY32W>() as u32;
-    println!("{:?}", pe32.dwSize);
 
     unsafe {
         if Process32FirstW(h_process_snap, pe32) == 0 {
@@ -63,14 +66,100 @@ fn get_processes() {
             let os_string = OsString::from_wide(&pe32.szExeFile[..]);
             // ExeFile names have a lot of trailing 0's, remove them...
             let exe_files: String = os_string.into_string().unwrap().replace("\u{0}", "");
-            println!("{:?}", exe_files);
+            
+            for file_name in exe_files.split("\n") {
+                println!("{}", file_name);
+            }
 
         }
     }
 
+    unsafe {
+        CloseHandle(h_process_snap);
+    }
+
 }
 
+// TODO:
+// duplicate code
+fn get_process_ids(process_name: &str) -> Vec<u32> {
+    let h_process_snap: HANDLE;
+    let mut process_ids = Vec::new();
+
+    let mut pe32 = &mut PROCESSENTRY32W {
+        dwSize: 0,
+        cntUsage: 0,
+        th32ProcessID: 0,
+        th32DefaultHeapID: 0,
+        th32ModuleID: 0,
+        cntThreads: 0,
+        th32ParentProcessID: 0,
+        pcPriClassBase: 0,
+        dwFlags: 0,
+        szExeFile: [0; 260],
+    };
+
+    unsafe {
+        h_process_snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    }
+
+    pe32.dwSize = size_of::<PROCESSENTRY32W>() as u32;
+
+    unsafe {
+        if Process32FirstW(h_process_snap, pe32) == 0 {
+            CloseHandle(h_process_snap);
+            println!("can't get a process snapshot");
+            // return false
+        }
+
+        while Process32NextW(h_process_snap, pe32) != 0 {
+            let os_string = OsString::from_wide(&pe32.szExeFile[..]);
+            let exe_file_string: String = os_string.into_string().unwrap().replace("\u{0}", "");
+
+            if exe_file_string == process_name {
+                process_ids.push(pe32.th32ProcessID)
+            }
+        }
+    }
+
+    unsafe {
+        CloseHandle(h_process_snap);
+    }
+
+    process_ids
+
+}
+
+/*
+*
+* Basic SuperF4 (https://github.com/stefansundin/superf4) implementation in Rust 
+* Usage: `cargo run -- {process name}
+* 
+*/
 fn main() {
-    get_processes()
+    let args = Cli::from_args();
+    let mut process_name = String::new();
+
+    if &args.process_name == "" {
+        get_all_processes();
+        println!("Enter a process name...");
+        io::stdin().read_line(&mut process_name).unwrap();
+        process_name = process_name.replace("\r", "").replace("\n", "");
+    } else {
+        process_name = args.process_name;
+    }
+
+    let process_ids = get_process_ids(&process_name);
+
+    if process_ids == Vec::new() {
+        println!("No process found called '{}'", process_name)
+    }
+
+    unsafe {
+        for process_id in process_ids {
+            kill_process(process_id);
+        }
+    }
+
 
 }
