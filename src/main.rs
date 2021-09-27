34b02c87547c247afe5538671c6bb259ecc49b20
Stef <627468@student.inholland.nl>
2021-09-28 00:11:27 +0200

    progress...

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use std::io;
 use std::mem::size_of;
 use std::ffi::OsString;
-use winapi::ctypes::wchar_t;
-use winapi::um::winnt::PROCESS_ALL_ACCESS;
-use winapi::um::psapi::GetProcessImageFileNameW;
+use winapi::ctypes::{wchar_t, c_char, c_void};
+use winapi::um::winnt::{PROCESS_ALL_ACCESS, PTOKEN_PRIVILEGES};
+use winapi::um::psapi::{GetProcessImageFileNameW, GetProcessImageFileNameA};
 use winapi::um::winnt::{HANDLE, LPCSTR, LPCWSTR, LPSTR, LPWSTR};
 use winapi::um::handleapi::CloseHandle;
 use std::os::windows::ffi::OsStringExt;
-use winapi::um::processthreadsapi::{TerminateProcess, OpenProcess};
+use winapi::um::processthreadsapi::{TerminateProcess, OpenProcess, GetProcessId};
 use winapi::um::tlhelp32::{Process32NextW, Process32FirstW, CreateToolhelp32Snapshot, TH32CS_SNAPPROCESS, PROCESSENTRY32W};
 use winapi;
-use winapi::shared::windef::HHOOK;
+use winapi::shared::windef::HWND;
 use winapi::um::winuser;
 use winapi::um::winuser::{GetWindowThreadProcessId, GetForegroundWindow, HC_ACTION, KBDLLHOOKSTRUCT, CallNextHookEx, WH_KEYBOARD_LL, WM_KEYDOWN, WM_SYSKEYDOWN, WM_SYSKEYUP, WM_KEYUP, VK_F4, VK_LMENU, VK_LCONTROL};
 use std::convert::TryFrom;
+use winapi::um::errhandlingapi::GetLastError;
+use winapi::um::securitybaseapi::AdjustTokenPrivileges;
 
 
 unsafe fn kill_process(process_id: u32) -> HANDLE {
@@ -36,15 +38,29 @@ unsafe fn kill_process(process_id: u32) -> HANDLE {
 fn main() {
     // https://github.com/linde12/winapi-testing !!!!
     unsafe {
+        /*
+        1. get HWND from getforegroundwindow
+        2. get DWORD processid from GetWindowThreadProcessId using hwnd
+        3. apparently, give ourselves permission using AdjustTokenPrivileges
+        4. give that pid to kill_process()
+        */
+
+        // 1
+        let handle: HWND = GetForegroundWindow();
+        // 2
         let mut pid: winapi::shared::minwindef::DWORD = 0;
-        let handle = GetForegroundWindow();
-        let kak = GetWindowThreadProcessId(handle, &mut pid);
-        let name: [wchar_t; 256] = [0; 256];
-        let mut nimma: u16 = 0;
+        pid = GetWindowThreadProcessId(handle, &mut pid);
+        // 3
         // TODO:
-        // GetLastError
-        // why 0?
-        let kaas = GetProcessImageFileNameW(handle as HANDLE, &mut nimma, name.len() as u32);
-        println!("{:?}", kaas);
+        // initialize these fuckers
+        let hToken: HANDLE;
+        let tkp: PTOKEN_PRIVILEGES;
+        let kak: winapi::shared::minwindef::PDWORD = &mut 0;
+
+        AdjustTokenPrivileges(hToken, winapi::shared::minwindef::FALSE, tkp, 0, tkp, kak);
+
+
+        // 4
+        kill_process(pid);
     }
 }
